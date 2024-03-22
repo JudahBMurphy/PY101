41994a4254de4bb2bdc59567109a5f826862f749
Judah Murphy <judahbaronmurphy@gmail.com>
2024-03-22 13:41:01 -0500

    Calculator updates part 2

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,12 +1,29 @@
-print('Welcome to Calculator')
+def prompt(message):
+    print(f'==> {message}')
+
+def invalid_number(user_input):
+    try:
+        int(user_input)
+    except ValueError:
+        return True
+    return False
+
+def test_input(user_input):
+    while invalid_number(user_input):
+        prompt("Not a valid number. Please try again.")
+        user_input = input()
+    return user_input
+
+
+prompt('Welcome to Calculator')
 
 # Ask the user for the first number.
-print('Please enter the first number:')
-first_number = int(input())
+prompt('Please enter the first number:')
+first_number = test_input(input())
 
 # Ask the user for the second number.
-print('Please enter the second number:')
-second_number = int(input())
+prompt('Please enter the second number:')
+second_number = test_input(input())
 
 #Ask the user for an operation to perform
 print('''What operation would you like to perform?
@@ -14,21 +31,23 @@ print('''What operation would you like to perform?
 2. Subtraction
 3. Multiplication
 4. Division''')
-operand = int(input())
-result = 0
+operand = test_input(input())
+while operand not in ['1', '2', '3', '4']:
+    prompt('Please choose from the given options 1, 2, 3, or 4')
+    operand = test_input(input())
 
 #Perform the operation on the two numbers
 match operand:
-	case 1:
-		result = first_number + second_number
-	case 2:
-		result = first_number - second_number
-	case 3:
-		result = first_number * second_number
-	case 4:
-		result = first_number // second_number
-	case _:
-		print('Input not recognized. Goodbye.')
+    case '1':
+        result = int(first_number) + int(second_number)
+    case '2':
+        result = int(first_number) - int(second_number)
+    case '3':
+        result = int(first_number) * int(second_number)
+    case '4':
+        result = int(first_number) // int(second_number)
+    case _:
+        print('Input not recognized. Goodbye.')
 
 #Print the result to the terminal
 print(f'Result is: {result}')
