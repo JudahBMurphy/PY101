74f02c1fca232a9868d86725d6861e2a9cf4ae97
Judah Murphy <judahbaronmurphy@gmail.com>
2024-03-22 19:46:47 -0500

    Added functionality to do additional calculations.'

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -17,37 +17,46 @@ def test_input(user_input):
 
 prompt('Welcome to Calculator')
 
-# Ask the user for the first number.
-prompt('Please enter the first number:')
-first_number = test_input(input())
+while True:
+    # Ask the user for the first number.
+    prompt('Please enter the first number:')
+    first_number = test_input(input())
 
-# Ask the user for the second number.
-prompt('Please enter the second number:')
-second_number = test_input(input())
+    # Ask the user for the second number.
+    prompt('Please enter the second number:')
+    second_number = test_input(input())
 
-#Ask the user for an operation to perform
-print('''What operation would you like to perform?
-1. Addition
-2. Subtraction
-3. Multiplication
-4. Division''')
-operand = test_input(input())
-while operand not in ['1', '2', '3', '4']:
-    prompt('Please choose from the given options 1, 2, 3, or 4')
+    #Ask the user for an operation to perform
+    prompt('''What operation would you like to perform?
+    1. Addition
+    2. Subtraction
+    3. Multiplication
+    4. Division''')
     operand = test_input(input())
+    while operand not in ['1', '2', '3', '4']:
+        prompt('Please choose from the given options 1, 2, 3, or 4')
+        operand = test_input(input())
 
-#Perform the operation on the two numbers
-match operand:
-    case '1':
-        result = int(first_number) + int(second_number)
-    case '2':
-        result = int(first_number) - int(second_number)
-    case '3':
-        result = int(first_number) * int(second_number)
-    case '4':
-        result = int(first_number) // int(second_number)
-    case _:
-        print('Input not recognized. Goodbye.')
+    #Perform the operation on the two numbers
+    match operand:
+        case '1':
+            result = int(first_number) + int(second_number)
+        case '2':
+            result = int(first_number) - int(second_number)
+        case '3':
+            result = int(first_number) * int(second_number)
+        case '4':
+            result = int(first_number) // int(second_number)
+        case _:
+            prompt('Input not recognized. Goodbye.')
 
-#Print the result to the terminal
-print(f'Result is: {result}')
+    #Print the result to the terminal
+    print(f'Result is: {result}')
+
+    #Prompt another calculation
+    prompt('Would you like to perform another calculation? (y/n)')
+    calc_again = input()
+    if calc_again and calc_again[0].lower() != 'y':
+        break
+
+    
